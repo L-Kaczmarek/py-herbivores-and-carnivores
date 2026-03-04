37408ff87d0c9e9cf982edfa8cdeb31e387346eb
Łukasz Kaczmarek <Lukasz.kaczmarek1993@wp.pl>
2026-03-04 18:05:45 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(self, name: str, health: int = 100, hidden: bool = False):
+        self.health = health
+        self.name = name
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self):
+        return f'{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}'
+
+
+
+class Herbivore(Animal):
+    def hide(self):
+        if self.hidden == False:
+            self.hidden = True
+        else:
+            self.hidden = False
+
+
+
+
+class Carnivore(Animal):
+    def bite(self, other):
+        if not isinstance(other, Carnivore):
+            if other.hidden == False:
+                other.health -= 50
+            if other.health <= 0:
+                Animal.alive.remove(other)
